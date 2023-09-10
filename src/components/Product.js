f73b8a596ea0d38208d88e8c7d944443867bebe2
Collins Kimutai <collinskimutai72@gmail.com>
2023-09-10 21:35:52 +0300

    added product list

---
+++ src/components/Product.js
@@ -0,0 +1,18 @@
+import '../styles/product.css';
+import React from 'react';
+import PropTypes from 'prop-types';
+function Product(props) {
+   
+   return (
+       <React.Fragment>
+           <p> {props.name} </p>
+           <p>Price - {props.price} </p>
+       </React.Fragment>
+   )
+}
+Product.propTypes ={
+    name: PropTypes.string,
+    Price: PropTypes.string
+ }
+
+export default Product
