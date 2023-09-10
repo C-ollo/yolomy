a367ff1152b4505b19bb3ff69d8b2de916ba5f22
Collins Kimutai <collinskimutai72@gmail.com>
2023-09-10 20:35:53 +0300

    add about us

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import '../styles/App.css';
 import Header from './Header';
+import AboutUs from './AboutUs';
 
 function App() {
  return (
     <React.Fragment>
       <div className="container-fluid">
-      <Header/>
+        <Header/>
+        <AboutUs />
       </div>
     </React.Fragment>  
  )
