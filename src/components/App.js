35966e35cad8cac57cea71806a038ff1206af826
Collins Kimutai <collinskimutai72@gmail.com>
2023-09-10 20:04:41 +0300

    initial

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../App.css';
+import Header from './Header';
+import Navbar from './Navbar';
+
+function App() {
+ return (
+    <React.Fragment>
+      <div>
+      <Header/>
+      </div>
+    </React.Fragment>  
+ )
+}
+
+export default App;
