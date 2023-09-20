02c5f7cbe575a7d9814908ee88e9d41fb007a525
Collins Kimutai <collinskimutai72@gmail.com>
2023-09-20 10:30:16 +0300

    add new product form

--- src/components/ProductControl.js
+++ src/components/ProductControl.js
@@ -1,19 +1,31 @@
 import React, { Component } from 'react';
 import ProductList from './ProductList';
 import AddProduct from './AddProduct';
+import NewProductForm from './NewProductForm';
 
 class ProductControl extends Component {
    constructor(props){
        super(props);
-       this.state ={}
+       this.state ={
+        productFormVisible: false
+    }
    }
    render() {
+    let currentVisibleState = null;
+       if (this.state.productFormVisible){
+           currentVisibleState = <NewProductForm />
+       }else{
+           currentVisibleState = <ProductList />
+       }
        return (
            <React.Fragment>
-            <AddProduct/>
+               <AddProduct />
+               {currentVisibleState}
            </React.Fragment>
        )
-   }
+    }
+       
+   
 }
 
 export default ProductControl
